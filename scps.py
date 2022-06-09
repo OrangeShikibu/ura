b40e5eff0695c9fb017a55f2a538bbcb658cddcb
OrangeShikibu <yumotomichiaki@gmail.com>
2022-06-09 19:34:20 +0900

    金研論文データセット作成
    とりあえず、金研の論文データセットを作成し、エクセルで出力するまでと、おおよその状況を把握するためのコードを入力

--- scps.py
+++ scps.py
@@ -7,11 +7,83 @@ from researchcapability import Scopus
 home = os.environ["HOME"]
 # データディレクトリのpathを設定
 data = "/data/scopus/20220608/"
-print(home)
+data1 = "/data/scopus/20220608/2021"
+data2 = "/data/scopus/20220608/2022"
+
 scps = Scopus()
 
-print(os.curdir)
-df = scps.mkDataFrame(home + data)
+# scopusデータの読み込み
+df1 = scps.mkDataFrame(home + data1)
+df2 = scps.mkDataFrame(home + data2)
+df = pd.concat([df1, df2])
 
-len(df)
-df
+# ===============================================================
+# 部局名辞書の読み込み
+dict1 = "/data/dict/current/dict_TU_Orgs_Name_20211203.xlsx"
+df_dics = pd.read_excel(home + dict1)
+## 金属材料研究所
+bukyokumei = ["IMR"]
+df_dics_bukyoku = df_dics[df_dics["Abbrevia"].isin(bukyokumei)]
+bukyokulist = []
+for i in df_dics_bukyoku["OrgsNameE"]:
+    # 正規表現に用いる記号を処理し、
+    # ', 'を先頭に付ける。
+    i = i.replace("(", "\(")
+    i = i.replace(")", "\)")
+    i = i.replace("-", "\-")
+    i = i.replace(".", "\.")
+    i = ", " + i
+    bukyokulist.append(i)
+bukyokunames = "|".join(bukyokulist)
+
+##
+bukyokumei2 = ["unknown"]
+df_dics_tu = df_dics[~df_dics["Abbrevia"].isin(bukyokumei2)]
+df_dics_tu.head()
+tulist = []
+for i in df_dics_tu["OrgsNameE"]:
+    # 正規表現に用いる記号を処理し、
+    # ', 'を先頭に付ける。
+    i = i.replace("(", "\(")
+    i = i.replace(")", "\)")
+    i = i.replace("-", "\-")
+    i = i.replace(".", "\.")
+    i = ", " + i
+    tulist.append(i)
+
+# 分析対象部局の組織名英語表記OrgsNameEを文字列
+tunames = "|".join(tulist)
+
+# ==============================================================
+# 金属材料研究所研究者のデータファイル
+imrresearchers = home + "/data/IMR/IMR_Researchers/IMR_researchers_20220415.xlsx"
+df_imr_researcher = pd.read_excel(imrresearchers)
+scpsid = []
+for i in df_imr_researcher["Scopus_AuthorID"][
+    df_imr_researcher["Scopus_AuthorID"] != "unknown"
+]:
+    i = str(i)
+    i = ";" + i + ";"
+    scpsid.append(i)
+scpsids = "|".join(scpsid)
+
+# 金属材料研究所　部局名辞書より抽出
+df_imr1 = df[df["著者 + 所属機関"].str.contains(bukyokunames, na=False)]
+
+# 部局未判明論文
+df_chk = df[~df["著者 + 所属機関"].str.contains(tunames, na=False)]
+plusstr = lambda i: ";" + i
+
+df_chk.columns
+df_chk["chk"] = df_chk["著者ID"].apply(plusstr)
+df_imr2 = df_chk[df_chk["chk"].str.contains(scpsids)]
+del df_imr2["chk"]
+
+df_imr2
+df_imr = pd.concat([df_imr1, df_imr2])
+df_imr = df_imr.drop_duplicates()
+
+df_imr
+df_imr.to_excel(home + "/Desktop/imr20212022.xlsx")
+
+df_imr.pivot_table(values="EID", columns=["文献タイプ"], index=["出版年"], aggfunc=len)
