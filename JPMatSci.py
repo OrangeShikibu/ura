df9efeaf5a0a88ec000fbf718ecff0918f677c06
OrangeShikibu <yumotomichiaki@gmail.com>
2022-07-17 21:47:54 +0900

    追記

--- JPMatSci.py
+++ JPMatSci.py
@@ -17,6 +17,9 @@ data = "/googleMyDrive/data/wos/JPMatSci20220708/"
 df_wos = wos.mkDataFrame(home + data)
 df_wos["doi"] = df_wos["DOI"].str.lower()
 df_wos = df_wos.astype({'Addresses': 'str'})
+df_wos['add'] = df_wos["Addresses"].str.lower()
+df_wos['add'] = df_wos['add'].str.replace(' ','')
+df_wos['add']
 
 df_wos.pivot_table(
     values="UT (Unique WOS ID)",
@@ -25,49 +28,64 @@ df_wos.pivot_table(
     aggfunc=len,
 )
 
+
 ##########################
 # 部局名辞書ファイルの読み込み
 ##########################
 # 現在の部局名辞書ファイル
 dict = "/googleMyDrive/data/dict/current/dict_TU_Orgs_Name_20220627.xlsx"
 df_dics = pd.read_excel(home + dict)
+df_dics['orgsnamee'] = df_dics['OrgsNameE'].str.lower()
+df_dics['orgsnamee'] = df_dics['orgsnamee'].str.replace(' ','')
+
 # 部局を確認できている部局名辞書のデータフレームを作成する
 abbrevia = []
 for i in df_dics["Abbrevia"]:
     abbrevia.append(i)
 
 abbrevia = set(abbrevia)
+
 ## 部局の確認を終えていないものなどを削除
-abbrevia.remove("unknown")
+#abbrevia.remove("unknown")
 abbrevia.remove("notTU")
 df_dics_tu = df_dics[df_dics["Abbrevia"].isin(abbrevia)]
+
 tulist = []
-for i in df_dics_tu["OrgsNameE"]:
+for i in df_dics_tu["orgsnamee"]:
     # 正規表現に用いる記号を処理し、
     # ', 'を先頭に付ける。
     i = i.replace("(", "\(")
     i = i.replace(")", "\)")
     i = i.replace("-", "\-")
     i = i.replace(".", "\.")
-    i = ", " + i
+    i1 = "," + i
+    i2 = "]" + i
+    tulist.append(i)
     tulist.append(i)
 
 # 分析対象部局の組織名英語表記OrgsNameEを文字列
 tunames = "|".join(tulist)
 
+df_wos_tu = df_wos[df_wos['add'].str.contains(tunames)]
+df_wos_tu.pivot_table(
+    values="UT (Unique WOS ID)",
+    columns=["Document Type"],
+    index=["Publication Year"],
+    aggfunc=len,
+)
 ###########################
 # IMR研究者データの読み込み
 ###########################
 # 金研研究者のデータファイル
 imrresearchers = "/googleMyDrive/data/IMR/IMR_Researchers/IMR_researchers_20220415.xlsx"
 df_imr_researcher = pd.read_excel(home + imrresearchers)
-df_imr_researcher
 # 金研研究者のResearcherID
 imr_researcherIDs = df_imr_researcher["ResearcherID"].values.tolist()
 # 金研研究者の名前
 imr_name = df_imr_researcher["English Name"].values.tolist()
 # 金研研究者のScopus著者ID
 imr_scopusIDs = df_imr_researcher["Scopus_AuthorID"].values.tolist()
+
 
 ##############################
 # 部局分析の準備
@@ -79,7 +97,8 @@ df_dics_bukyoku = df_dics[df_dics["Abbrevia"].isin(bukyokumei)]
 # 部局WoSデータセットの作成
 ## 部局名文字列作成
 bukyokulist = []
-for i in df_dics_bukyoku["OrgsNameE"]:
+for i in df_dics_bukyoku["orgsnamee"]:
+    #for i in df_dics_bukyoku["OrgsNameE"]:
     # 正規表現に用いる記号を処理し、
     # ', 'を先頭に付ける。
     i = i.replace("(", "\(")
@@ -87,17 +106,24 @@ for i in df_dics_bukyoku["OrgsNameE"]:
     i = i.replace("-", "\-")
     i = i.replace(".", "\.")
     # imrlist2.append(i)
-    i1 = ", " + i
-    i2 = "] " + i
+    #i1 = ", " + i
+    #i2 = "] " + i
+    i1 = "," + i
+    i2 = "]" + i
     bukyokulist.append(i1)
     bukyokulist.append(i2)
+len(bukyokulist)
+bukyokulist = sorted(set(bukyokulist))
+len(bukyokulist)
 # 部局名辞書の表記を文字列化
 bukyokunames = "|".join(bukyokulist)
 # 部局名辞書により抽出したscopusデータセット
 # 列「著者＋所属機関」に部局名表記を含むものを抽出
-df_tmp = df_wos[df_wos["Addresses"].str.contains(bukyokunames, na=False)]
+#df_tmp = df_wos[df_wos["Addresses"].str.contains(bukyokunames, na=False)]
+df_tmp = df_wos[df_wos["add"].str.contains(bukyokunames, na=False)]
 # 部局名辞書で抽出された論文データフレーム（念のため重複を削除しておく）
 df_wos_bukyoku = df_tmp.drop_duplicates()
+
 # なお、金研を対象とするWoSの文献データフレームには、所属機関名が金研であっても、著者が金研に所属していない研究者ではない場合がありました。その文献書誌情報を見ると所属機関名の表記の区切り方が間違えていることを確認しています。そのような文献書誌情報を金研の文献データフレームから削除する必要があることから以下の操作を行っています。
 # 金研ではない所属機関情報のものを除く
 if bukyokumei == "IMR":
@@ -108,26 +134,19 @@ if bukyokumei == "IMR":
         ~df_wos_bukyoku["Addresses"].str.contains(notIMR[0])
     ]
 
-# ### 部局名辞書を使って、部局不明の論文データフレーム を作成
-tulist = []
-for i in df_dics_tu["OrgsNameE"]:
-    i = i.replace("(", "\(")
-    i = i.replace(")", "\)")
-    i = i.replace("-", "\-")
-    i = i.replace(".", "\.")
-    # i = ', '+i
-    tulist.append(i)
-tunames = "|".join(tulist)
+df_wos_bukyoku['UT (Unique WOS ID)']
 
-## 部局が判明していないもののデータフレームを作成
-df_tmp = df_wos[~df_wos["Addresses"].str.contains(tunames, na=False)]
-df_wos_other = df_tmp.drop_duplicates()
+# 金研以外の論文
+df_wos_other = df_wos[~df_wos['UT (Unique WOS ID)'].isin(df_wos_bukyoku['UT (Unique WOS ID)'])]
+
+
 # 部局不明論文データフレーム から金研論文をResearcherIDを使って抽出
 imr_researcherIDs = sorted(set(imr_researcherIDs))
 imr_researcherIDs.remove("unknown")
 imrresearcherids = "|".join(imr_researcherIDs)
 # ResearcherIDで抽出
-df_wos_imr2 = df_wos_other[df_wos_other["Researcher Ids"].str.contains(imrresearcherids, na=False)]
+df_wos_imr2t = df_wos_other[df_wos_other["Researcher Ids"].str.contains(imrresearcherids, na=False)]
+df_wos_imr2 = df_wos_imr2t[df_wos_imr2t["add"].str.contains('tohokuuniv')]
 # 著者名で抽出
 # 名前の大文字・小文字調整
 namelist = []
@@ -141,11 +160,14 @@ for i in namelist:
     namelist2.append(i)
 researchernames = "|".join(namelist2)
 
-df_wos_other2 = df_wos_other[~df_wos_other["UT (Unique WOS ID)"].isin(df_wos_imr2["UT (Unique WOS ID)"])]
+df_wos_other2 = df_wos_other[~df_wos_other["UT (Unique WOS ID)"].isin(df_wos_imr2t["UT (Unique WOS ID)"])]
 df_wos_imr3tmp = df_wos_other2[df_wos_other2["Author Full Names"].str.contains(researchernames, na=False)]
 researchernames2 = "Niinomi, Mitsuo|Sugiyama, Kazumasa|Nakajima, Kazuo|Shimada, Yusuke|Yoshikawa, Akira|Semboshi, Satoshi"
 df_wos_imr3 = df_wos_imr3tmp[df_wos_imr3tmp["Author Full Names"].str.contains(researchernames2, na=False)]
-df_wos_imr3.to_excel("/Users/yumoto/Desktop/wos_著者名抽出.xlsx")
+
+df_wos_bukyoku.to_excel(home + "/Desktop/wos_imr1.xlsx")
+df_wos_imr2.to_excel(home + "/Desktop/wos_imr2.xlsx")
+df_wos_imr3.to_excel(home + "/Desktop/wos_imr3.xlsx")
 # ### WoSデータフレーム の統合
 df_wos_imr = pd.concat([df_wos_bukyoku, df_wos_imr2, df_wos_imr3])
 df_wos_imr = df_wos_imr.drop_duplicates()
